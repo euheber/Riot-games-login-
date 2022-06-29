d67c2742757b1bde3e33446c6d002f97bd3f58db
euheber <euheber1@gmail.com>
2022-06-29 20:28:33 -0300

    finished: darkmode

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,21 @@
+const lightsBtn = document.querySelector('#darkMode')
+const loginBody = document.querySelector('#login-body')
+const h1 = document.querySelector('h1')
+const label = document.querySelector('label')
+const r = document.querySelector(':root')
 
+lightsBtn.addEventListener('click', ()=> { 
+    loginBody.classList.toggle('on')
+
+   if(loginBody.classList.contains('on')){
+    h1.style.color = 'white'
+    label.style.color = 'white'
+    r.style.setProperty('--light', 'grey');
+    r.style.setProperty('--dark', '#ffbb00');
+   } else { 
+    h1.style.color = 'black'
+    label.style.color = 'black'
+    r.style.setProperty('--light', '#ffbb00');
+    r.style.setProperty('--dark', 'black');
+   }
+})
