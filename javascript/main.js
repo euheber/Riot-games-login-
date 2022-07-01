b3d67a262dfecf833c6414a7027847e5921a4df5
euheber <euheber1@gmail.com>
2022-07-01 20:57:40 -0300

    background in body test

--- javascript/main.js
+++ javascript/main.js
@@ -40,8 +40,8 @@ function change(){
    const rand =  Math.floor(Math.random() * 3 + 0)
    const bg = `url(${bgs[rand]})`
    console.log(bg);
-   background.style.backgroundImage = bg
-   background.style.backgroundSize = 'cover'
+   body.style.backgroundImage = bg
+   body.style.backgroundSize = 'cover'
 }
 
 setInterval(change, 25000)
